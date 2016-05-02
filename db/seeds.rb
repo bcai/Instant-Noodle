c6d419adab0aefcaf07ee4be7091a3099b57525a
bcai <bcai916@gmail.com>
2016-05-01 19:41:08 -0700

    reseed database, fix Cloudinary optimization

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create!([
+  {username: 'guest', full_name: "Guest", password: "password"}
+  {username: 'bcai', full_name: "Brando Cai", password: "password"},
   {username: 'dtru', full_name: "Devina Tru", password: "password"},
   {username: 'dtruong', full_name: "Dev Tru", password: "password"},
-  {username: 'bcai', full_name: "Brando Cai", password: "password"},
   {username: 'branderf', full_name: "Brandino Acai", password: "password"},
   {username: 'bderf', full_name: "Bran Don", password: "password"},
   {username: 'bDiddy', full_name: "DonBranCai", password: "password"},
-  {username: 'guest', full_name: "Guest", password: "password"}
 ])
 
 Post.create!([
-  {description: 'Soup of the day', author_id: 3, image_url: 'https://images.unsplash.com/photo-1449793079811-bc4bcd990abf?crop=entropy&fit=crop&fm=jpg&h=850&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1450'},
-  {description: 'My favorite pie. #cranberry', author_id: 3, image_url: 'https://images.unsplash.com/photo-1460380410874-537ecece3984?crop=entropy&fit=crop&fm=jpg&h=850&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1450'},
-  {description: '#spicy', author_id: 2, image_url: 'https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?crop=entropy&fit=crop&fm=jpg&h=850&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1450'},
-  {description: 'Best burger in town. #bacon #datcheesedoe', author_id: 1, image_url: 'https://images.unsplash.com/photo-1460306855393-0410f61241c7?crop=entropy&fit=crop&fm=jpg&h=850&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1450'},
+  {description: 'Best burger in town. #bacon #datcheesedoe', author_id: 1, image_url: 'http://res.cloudinary.com/bcai/image/upload/s--DgWsq3AF--/q_50/v1462156042/photo-1460306855393-0410f61241c7_rj67s8.webp'},
+  {description: '#spicy', author_id: 4, image_url: 'http://res.cloudinary.com/bcai/image/upload/s--bizMKI0P--/q_50/v1462156091/photo-1455619452474-d2be8b1e70cd_svy3ux.webp'},
+  {description: 'My favorite pie. #cranberry', author_id: 1, image_url: 'http://res.cloudinary.com/bcai/image/upload/s--ONJlQtpw--/q_50/v1462156107/photo-1460380410874-537ecece3984_ay4cki.webp'},
+  {description: 'Spaghetti #simple #delicious', author_id: 2, image_url: 'http://res.cloudinary.com/bcai/image/upload/s--HcIipXJe--/q_50/v1462156331/photo-1458644267420-66bc8a5f21e4_wavmps.webp'},
+  {description: 'Soup of the day', author_id: 2, image_url: 'http://res.cloudinary.com/bcai/image/upload/s--gwl6gtra--/q_50/v1462156129/photo-1449793079811-bc4bcd990abf_hiwd1y.webp'}
 ])
--- frontend/components/upload_button.jsx
+++ frontend/components/upload_button.jsx
@@ -5,13 +5,10 @@ module.exports = React.createClass({
     event.preventDefault();
 
     cloudinary.openUploadWidget(
-      window.cloudinary_options, 
+      window.cloudinary_options,
       function(error, images){
         if (error == null) {
-          var fullUrl = images[0].secure_url;
-          var urlPath = ("f_auto/" + images[0].public_id + "." + images[0].format);
-          fullUrl = fullUrl.replace(images[0].path, urlPath);
-          debugger
+          var fullUrl = images[0].thumbnail_url;
           this.props.addImage(fullUrl);
         }
     }.bind(this));
