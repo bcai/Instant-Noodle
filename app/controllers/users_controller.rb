37cf7b93fb9a665df7d7f40a4838ec025141c563
bcai <bcai916@gmail.com>
2016-05-04 19:20:42 -0700

    add styling, fetchAllUsers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render "index.json.jbuilder"
+  end
+
   def new
     @user = User.new
   end
--- frontend/stores/user_store.js
+++ frontend/stores/user_store.js
@@ -5,6 +5,7 @@ var AppDispatcher = require('../dispatcher/dispatcher.js');
 var UserStore = new Store(AppDispatcher);
 
 var _user = {};
+var _allUsers = {};
 
 
 var resetUser = function (user) {
@@ -17,11 +18,24 @@ UserStore.user = function () {
   return _user;
 }
 
+// var resetAllUsers = function (users) {
+//   _users = {};
+// }
+
+UserStore.allUsers = function () {
+  return Object.keys(_allUsers).map(function(userId){
+    return _allUsers[userId];
+  });
+}
+
 
 UserStore.__onDispatch = function (payload) {
   switch (payload.actionType) {
     case UserConstants.USER_RECEIVED:
       resetUser(payload.user);
+      break;
+    case UserConstants.USERS_RECEIVED:
+      resetAllUsers(payload.users);
       break;
   }
 };
